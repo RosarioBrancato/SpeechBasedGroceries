fc0133e26aeaa026ae04ca4f7b3f1c5588525c81
RomanBoegli <roman-boegli@hotmail.com>
2020-05-02 18:40:47 +0200

    progress in Orderer

--- SpeechBasedGroceries/BusinessLogic/Orderer.cs
+++ SpeechBasedGroceries/BusinessLogic/Orderer.cs
@@ -1,5 +1,8 @@
 ﻿using SpeechBasedGroceries.DTOs;
 using SpeechBasedGroceries.Parties.CRM;
+using SpeechBasedGroceries.Parties.Bank;
+using SpeechBasedGroceries.Parties.Logistics;
+using SpeechBasedGroceries.Parties.Fridgy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,26 +12,101 @@ namespace SpeechBasedGroceries.BusinessLogic
 {
 	public class Orderer
 	{
+		private Customer customer;
+		private BankClient bankClient = new BankClient();
+		private LogisticsClient logisticsClient = new LogisticsClient();
+		private FridgyClient fridgyClient = new FridgyClient();
 
-		public bool LoginWithTelegram(string telegramId)
-		{
-			//TO-DO
-			return false;
+        public Orderer(string telegramId)
+        {
+			this.fridgyClient = new FridgyClient(new Registrar().GetFridgyToken(telegramId).Value);
+			this.bankClient = new BankClient();
+			this.logisticsClient = new LogisticsClient();
+
+			this.customer = new CrmClient().GetCustomerByTelegramId(telegramId);
 		}
 
-		public bool PlaceOrder()
+
+        /* 
+         *  Returns a Delivery if successful and Null if unsuccessful
+         */
+		public Delivery PlaceOrder(Product product, int amount, string comment = "")
 		{
-			//TO-DO
 
-			//CRM: get user personal info (name, address etc.)
-			//BANK: check balance
-			//BANK: edit balance
-			//LOG: place order in logistics
-			//FRIDGY: place items in fridge
-			//BOT: return confirmation
+			Delivery delivery = null;
+            if (this.customer != null)
+            {
 
-			return true;
+				bool success = TryPayment(customer, product, amount);
+
+				if (success)
+                {
+					delivery = DispatchDelivery(customer, product, amount, comment);
+					
+                } else
+				{
+                    // not enough funds (could technically get here...)
+                }
+
+			} else
+            {
+                // customer does not exist
+                // should never get here because of constructor
+            }
+
+			return delivery;
 		}
+
+
+        private bool TryPayment(Customer customer, Product product, int amount)
+        {
+			// issue fictional transaction
+			Transaction transaction = GetTransaction(product, amount);
+
+            // try if transaction can be placed
+			return bankClient.IssuePayment(customer.Id, transaction);
+		}
+
+
+        private Transaction GetTransaction(Product product, int amount)
+        {
+            // TODO: how to get the real pricing?
+            return bankClient.GetRandomTransaction();
+		}
+
+
+        private Delivery DispatchDelivery(Customer customer, Product product, int amount, string comment)
+        {
+            // currently every delivery has only one position
+
+			Delivery delivery = new Delivery
+			{
+				CustomerId = customer.Id,
+				Date = DateTime.Now,
+				Street = customer.Street,
+				Zip = customer.Zip,
+				City = customer.City,
+				Country = customer.Country,
+				Comment = comment
+			};
+
+			delivery.Positions = new List<Position>
+					{
+						new Position
+						{
+							No = 10,
+							ItemId = product.Barcode,
+							ItemText = product.Name,
+							ItemQty = amount,
+							ItemPrice = 0,
+							ItemWeight = 0,
+							Comment = ""
+						}
+					};
+
+			return logisticsClient.CreateUpdateDelivery(delivery, true);
+		}
+        
 
 	}
 }
---
+++ SpeechBasedGroceriesTest/BusinessLogic/OrdererTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpeechBasedGroceries.BusinessLogic;
+using SpeechBasedGroceries.DTOs;
+using SpeechBasedGroceries.Parties.Fridgy;
+using SpeechBasedGroceries.Parties.Logistics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeechBasedGroceries.BusinessLogic.Tests
+{
+    [TestClass()]
+    public class OrdererTests
+    {
+        private Orderer oderer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.oderer = new Orderer("356131678");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.oderer = null;
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTest()
+        {
+            string barcode = "7610200428059"; // Bratwurst
+
+            Product product = new FridgyClient().GetProductByBarcode(barcode); 
+            Delivery delivery = oderer.PlaceOrder(product, 1, "Eine Bestellung");
+
+            Assert.IsNotNull(delivery);
+            Assert.IsTrue(delivery.Positions[0].ItemId.Equals(barcode));
+        }
+    }
+}
